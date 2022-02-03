49d9d46357116e8661a0d4a89423dbccd69d2080
Curvin100 <84587842+Curvin100@users.noreply.github.com>
2022-02-03 16:40:15 -0500

    Add files via upload

---
+++ Hwk_3.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 29 15:33:05 2022
+
+@author: kevin
+"""
+
+def text_to_morse(translate):
+    B = ('-...')
+    E = ('.')
+    I = ('..')
+    K = ('-.-')
+    N = ('-.')
+    O = ('---')
+    R = ('.-.')
+    T = ('-')
+    V = ('...')
+    if Letter == 'B':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), B))
+    elif Letter == 'E':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), E))
+    elif Letter == 'I':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), I))
+    elif Letter == 'K':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), K))
+    elif Letter == 'N':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), N))
+    elif Letter == 'R':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), R))
+    elif Letter == 'O':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), O))
+    elif Letter == 'T':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), T))
+    elif Letter == 'V':
+        return ('The morse code for {0} is, {1}'.format(Letter.upper(), V))
+    else:
+        return ('Unknown Character')
+
+
+intro = ("""
+      Welcome to a basic Morse Code Simulator. This will allow you to write 
+      basic messages into Morse Code!
+      """)
+
+print(intro)
+
+index = 6
+while index > 0:
+    Letter = input('Please enter an uppercase letter: ')
+    morse_code = text_to_morse(Letter)
+    print(morse_code)
+    index -= 1
